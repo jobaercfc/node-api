b5b728b1e5272a87de933fddfcc3d857c3f0a4d8
Abdullah Al Jobaer <surid@Abdullahs-MacBook-Air.local>
2020-02-04 15:52:38 +0900

    minor changes

--- controllers/post.js
+++ controllers/post.js
@@ -1,7 +1,13 @@
 const Post = require('../models/post')
 
 exports.getPosts = (req, res) => {
-    
+    const post = Post.find().select("_id, title body")
+    .then((posts) => {
+        res.json({
+            posts: posts
+        });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.createPost = (req, res) => {
